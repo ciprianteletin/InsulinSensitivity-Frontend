02f4b4c0a0768617301b77eafee2b78dc950ce63
Ciupix22 <ciprian.teletin99@e-uvt.ro>
2021-02-12 17:42:53 +0200

    Added index component and new dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { HeaderComponent } from './components/header/header.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-import { RegisterDetailsComponent } from './components/register-details/register-details.component';
-import { MainHeaderComponent } from './components/main-header/main-header.component';
-import { PrincipalComponent } from './components/principal/principal.component';
-import { InsulinFormComponent } from './components/insulin-form/insulin-form.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {FooterComponent} from './components/footer/footer.component';
+import {HeaderComponent} from './components/header/header.component';
+import {LoginComponent} from './components/login/login.component';
+import {RegisterComponent} from './components/register/register.component';
+import {RegisterDetailsComponent} from './components/register-details/register-details.component';
+import {PrincipalComponent} from './components/principal/principal.component';
+import {InsulinFormComponent} from './components/insulin-form/insulin-form.component';
+import { IndexComponent } from './components/index/index.component';
 
 @NgModule({
   declarations: [
@@ -20,15 +20,16 @@ import { InsulinFormComponent } from './components/insulin-form/insulin-form.com
     LoginComponent,
     RegisterComponent,
     RegisterDetailsComponent,
-    MainHeaderComponent,
     PrincipalComponent,
-    InsulinFormComponent
+    InsulinFormComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
