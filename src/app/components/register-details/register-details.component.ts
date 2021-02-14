ff48bf95eecd6281d84067165dfa333333fe8eca
Ciupix22 <ciprian.teletin99@e-uvt.ro>
2021-02-14 10:04:59 +0200

    Migrated duplicate CSS into a separated file

--- src/app/components/register-details/register-details.component.ts
+++ src/app/components/register-details/register-details.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-register-details',
   templateUrl: './register-details.component.html',
-  styleUrls: ['./register-details.component.css']
+  styleUrls: ['./register-details.component.css', '../../../assets/styles/login-register.css']
 })
 export class RegisterDetailsComponent implements OnInit {
 
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css', '../../../assets/styles/login-register.css']
 })
 export class RegisterComponent implements OnInit {
 
